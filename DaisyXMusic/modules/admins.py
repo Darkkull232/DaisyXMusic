2f5f9ab369d853787dd03a3aa385cbf315311ece
Darkkull232 <91105831+Darkkull232@users.noreply.github.com>
2021-09-21 10:53:04 +0530

    Update admins.py

--- DaisyXMusic/modules/admins.py
+++ DaisyXMusic/modules/admins.py
@@ -20,13 +20,14 @@ from asyncio import QueueEmpty
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-from DaisyXMusic.function.admins import set
-from DaisyXMusic.helpers.channelmusic import get_chat_id
-from DaisyXMusic.helpers.decorators import authorized_users_only, errors
-from DaisyXMusic.helpers.filters import command, other_filters
-from DaisyXMusic.services.callsmusic import callsmusic
-from DaisyXMusic.services.queues import queues
-from DaisyXMusic.config import que
+from Darkskull.function.admins import set
+from Darkskull.helpers.channelmusic import get_chat_id
+from Darkskull.helpers.decorators import authorized_users_only, errors
+from Darkskull.helpers.filters import command, other_filters
+from Darkskull.services.callsmusic import callsmusic
+from Darkskull.services.queues import queues
+from Darkskull.services.queues import queues
+from Darkskull.config import que
 
 
 @Client.on_message(filters.command("adminreset"))
